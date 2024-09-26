9ab9b88fb1a2d75f71d1b4db8daf616269fb0207
nestorraulcg <nestorraulcgomez@gmail.com>
2024-09-25 23:26:56 -0300

    SEO: title and metatagswere added

--- src/app/pages/pricing-page/pricing-page.component.ts
+++ src/app/pages/pricing-page/pricing-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   standalone: true,
@@ -6,4 +7,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './pricing-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PricingPageComponent { }
+export default class PricingPageComponent implements OnInit {
+
+  private title = inject(Title);
+  private meta = inject(Meta);
+
+  ngOnInit(): void {
+    this.title.setTitle('Pricing Page');
+    this.meta.updateTag({ name: 'description', content: 'Pricing Page Description' });
+    this.meta.updateTag({ name: 'og:title', content: 'Pricing Page' });
+    this.meta.updateTag({ name: 'keywords', content: 'Hola, Mundo, Nestor' });
+  }
+
+}
