2fed810e6f0d9909fc343df75eb6309c9c428f8b
nestorraulcg <nestorraulcgomez@gmail.com>
2024-10-07 23:00:28 -0300

    pokemon list skeleton created

--- src/app/pages/pokemons/pokemons-page.component.ts
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -1,11 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ApplicationRef, ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { PokemonListComponent } from '../../pokemons/components/pokemon-list/pokemon-list.component';
+import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton.component";
 
 @Component({
   selector: 'pokemons-page',
   standalone: true,
-  imports: [PokemonListComponent],
+  imports: [PokemonListComponent, PokemonListSkeletonComponent],
   templateUrl: './pokemons-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PokemonsPageComponent { }
+export default class PokemonsPageComponent implements OnInit, OnDestroy {
+
+  // public isLoading = signal(true);
+
+  // private appRef = inject(ApplicationRef);
+
+  // private $appState = this.appRef.isStable.subscribe((isStable) => {
+  //   console.log({ isStable });
+
+  // });
+
+  ngOnInit() {
+    // setTimeout(() => {
+    //   this.isLoading.set(false);
+    // }, 5000);
+  }
+
+  ngOnDestroy() {
+    // this.$appState.unsubscribe();
+  }
+
+}
